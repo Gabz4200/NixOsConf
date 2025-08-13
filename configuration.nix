d611be521955ff35a7923ad2219a2617a46b3b8b
Gabriel Amaral <jgsa75005@gmail.com>
2025-08-12 22:04:59 -0300

    fix: probably a working state

--- configuration.nix
+++ configuration.nix
@@ -241,6 +241,9 @@
   services.displayManager.sddm.enableHidpi = true;
   services.displayManager.sddm.wayland.compositor = "kwin";
   services.displayManager.sddm.extraPackages = [
+    pkgs.kdePackages.qtsvg
+    pkgs.kdePackages.qtmultimedia
+    pkgs.kdePackages.qtvirtualkeyboard
     (pkgs.sddm-astronaut.override {embeddedTheme = "black_hole";})
   ];
   services.displayManager.sddm.theme = "sddm-astronaut-theme";
@@ -395,6 +398,10 @@
   ];
 
   nix.nixPath = ["nixpkgs=${inputs.nixpkgs}"];
+
+  nix.settings.substituters = ["https://chaotic-nyx.cachix.org" "https://nix-community.cachix.org" "https://hyprland.cachix.org" "https://numtide.cachix.org" "https://cache.nixos.org/" ];
+  nix.settings.trusted-public-keys = [ "chaotic-nyx.cachix.org-1:HfnXSw4pj95iI/n17rIDy40agHj12WfF+Gqk6SonIT8=" "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs=" "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc=" "numtide.cachix.org-1:2ps1kLBUWjxIneOy1Ik6cQjb41X0iXVXeHigGmycPPE=" "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY=" ];
+  
 
   # XDG
   xdg.terminal-exec.enable = true;
