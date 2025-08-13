84fa346bec01c9e73ed82a18a093e2b8e662faab
Gabriel Amaral <jgsa75005@gmail.com>
2025-08-13 17:19:23 -0300

    fix: another attempt to fix internet

--- configuration.nix
+++ configuration.nix
@@ -221,12 +221,11 @@
     efi.canTouchEfiVariables = true;
   };
 
-  #FIXME: The kernel is downgraded to stable due to rtl8821ce breaking.
   # Kernel
   boot.kernelPackages = pkgs.linuxKernel.packages.linux_zen;
 
   # Wifi driver
-  boot.kernelModules = ["8821ce"];
+  #boot.kernelModules = ["8821ce"];
   #boot.extraModulePackages = [
   #  # pkgs-stable.linuxKernel.packages.linux_zen.rtl8821ce
   #];
@@ -439,9 +438,8 @@
 
   nix.nixPath = ["nixpkgs=${inputs.nixpkgs}"];
 
-  nix.settings.substituters = ["https://chaotic-nyx.cachix.org" "https://nix-community.cachix.org" "https://hyprland.cachix.org" "https://numtide.cachix.org" "https://cache.nixos.org/" ];
-  nix.settings.trusted-public-keys = [ "chaotic-nyx.cachix.org-1:HfnXSw4pj95iI/n17rIDy40agHj12WfF+Gqk6SonIT8=" "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs=" "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc=" "numtide.cachix.org-1:2ps1kLBUWjxIneOy1Ik6cQjb41X0iXVXeHigGmycPPE=" "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY=" ];
-  
+  nix.settings.substituters = ["https://chaotic-nyx.cachix.org" "https://nix-community.cachix.org" "https://hyprland.cachix.org" "https://numtide.cachix.org" "https://cache.nixos.org/"];
+  nix.settings.trusted-public-keys = ["chaotic-nyx.cachix.org-1:HfnXSw4pj95iI/n17rIDy40agHj12WfF+Gqk6SonIT8=" "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs=" "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc=" "numtide.cachix.org-1:2ps1kLBUWjxIneOy1Ik6cQjb41X0iXVXeHigGmycPPE=" "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="];
 
   # XDG
   xdg.terminal-exec.enable = true;
