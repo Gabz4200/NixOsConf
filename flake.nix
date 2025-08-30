f934c6c23cc57d5b946da5edb0ba6f67c6893875
Gabriel Amaral <jgsa75005@gmail.com>
2025-08-30 15:15:30 -0300

    feat: Substituters on flake level

--- flake.nix
+++ flake.nix
@@ -112,8 +112,10 @@
       "https://niri.cachix.org"
       "https://chaotic-nyx.cachix.org"
       "https://nix-gaming.cachix.org"
+      "https://nur.cachix.org"
       "https://cache.nixos.org"
       "https://nix-community.cachix.org"
+      "https://pre-commit-hooks.cachix.org"
       "https://catppuccin.cachix.org"
     ];
 
@@ -121,8 +123,10 @@
       "niri.cachix.org-1:Wv0OmO7PsuocRKzfDoJ3mulSl7Z6oezYhGhR+3W2964="
       "chaotic-nyx.cachix.org-1:HfnXSw4pj95iI/n17rIDy40agHj12WfF+Gqk6SonIT8="
       "nix-gaming.cachix.org-1:nbjlureqMbRAxR1gJ/f3hxemL9svXaZF/Ees8vCUUs4="
+      "nur.cachix.org-1:F8+2oprcHLfsYyZBCsVJZJrPyGHwuE+EZBtukwalV7o="
       "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+      "pre-commit-hooks.cachix.org-1:Pkk3Panw5AW24TOv6kz3PvLhlH8puAsJTBbOPmBo7Rc="
       "catppuccin.cachix.org-1:noG/4HkbhJb+lUAdKrph6LaozJvAeEEZj4N732IysmU="
     ];
   };
